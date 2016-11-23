af207345d81011066b62ddf6caae07a007623b96
Alexandre Brocard <a.brocard@ntymail.com>
2016-11-23 17:03:22 +0100

    gem faker + 'dependent': :destroy in models associations (Bet + Choice + User + UserChoice) + db/seedl OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :bets
-  has_many :user_choices
+  has_many :bets, dependent: :destroy
+  has_many :user_choices, dependent: :destroy
 
   validates :user_name, presence: true, uniqueness: true
 end
--- app/models/user_choice.rb
+++ app/models/user_choice.rb
@@ -2,5 +2,5 @@ class UserChoice < ApplicationRecord
   belongs_to :user
   belongs_to :choice
 
-  validates :choice, :user, :bet_amount, presence: true
+  validates :choice, :user, presence: true
 end
