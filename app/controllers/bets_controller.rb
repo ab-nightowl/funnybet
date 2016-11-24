b1713923e34c37f7d1c8f937b769f157fa02303d
hatchi julien <julien.hatchi@gmail.com>
2016-11-24 11:13:36 +0100

    bet-view without style and spinner

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,23 +1,47 @@
 class BetsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+  before_action :find_bet, only: [:show]
 
   def index
+    @bets = Bet.all
   end
 
   def show
   end
 
   def new
+    @bet = Bet.new
   end
 
   def create
+    @bet = Bet.new(bet_param)
+    if @bet.save
+    else
+      render @bet
+    end
   end
 
   def edit
   end
 
   def update
+    @bet.update
   end
 
+
+  private
+
+  def find_bet
+    @bet = Bet.find(params[:id])
+  end
+
+  def user_bet
+    @bet = Bet.find(params[:user_id])
+  end
+
+  def bet_params
+    params.require(:bet).permit(:user, :tilte)
+  end
 
 
 
