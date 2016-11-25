58853e006c2e80019a532e974ad9c07db81fb4af
hatchi julien <julien.hatchi@gmail.com>
2016-11-25 18:31:53 +0100

    modifs du seed mais ne marche pas

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,254 +38,213 @@ categories_names.each do |category|
 end
 
 
+ongoing_bet1 = Bet.new(
+  title: "Qui gagnera les élections US 2016 ?",
+  user: users.sample,
+  category: Category.where(name: "Politique").first,
+  finish_at: DateTime.new(2016,11,9,1,54,3)
+  #proof: ""
+)
+ongoing_bet1.choices.create(
+  title: "Sylvain sera en avance (avant 9h)",
+  winning: false
+)
+ongoing_bet1.choices.create(
+  title: "Sylvain sera à l'heure pile poils",
+  winning: false
+)
+ongoing_bet1.choices.create(
+  title: "Sylvain sera en retard (comme d'hab)",
+  winning: false
+)
+ongoing_bet1.save
+
+
+ongoing_bet2 = Bet.new(
+  title: "Qui gagnera les présidentielles 2017 ?",
+  user: users.sample,
+  category: Category.where(name: "Politique").first,
+  finish_at: DateTime.new(2017,5,7,22,30,0)
+  #proof: ""
+)
+ongoing_bet2.choices.create(
+  title: "Fillon",
+  winning: false
+)
+ongoing_bet2.choices.create(
+  title: "Juppé",
+  winning: false
+)
+ongoing_bet2.choices.create(
+  title: "MLP",
+  winning: false
+)
+ongoing_bet2.choices.create(
+  title: "Mélenchon",
+  winning: false
+)
+ongoing_bet2.save
+
+
+ongoing_bet3 = Bet.new(
+  title: "Julien est-il un black gay ?",
+  user: users.sample,
+  category: Category.where(name: "Privé").first,
+  finish_at: DateTime.new(2016,12,2,19,0,0),
+  #proof: "",
+  challenge_title: "Kiss kiss !",
+  challenge: "Si tu te trompes, tu devras embrasser Julien sur la bouche (dans n'importe quel cas) !"
+)
+ongoing_bet3.choices.create(
+  title: "Oui",
+  winning: false
+)
+ongoing_bet3.choices.create(
+  title: "Non",
+  winning: false
+)
+ongoing_bet3.choices.create(
+  title: "Il est bi !",
+  winning: false
+)
+ongoing_bet3.save
+
+
+ongoing_bet4 = Bet.new(
+  title: "Ronaldo va-t-il gagner le Ballon d'or ?",
+  user: users.sample,
+  category: Category.where(name: "Sports").first,
+  finish_at: DateTime.new(2017,9,1,19,0,0)
+)
+ongoing_bet4.choices.create(
+  title: "Oui",
+  winning: false
+)
+ongoing_bet4.choices.create(
+  title: "Non",
+  winning: false
+)
+
+
+ongoing_bet5 = Bet.new(
+  title: "Yvon va-t-il se battre avec un Bulgare avant la fin de la formation ?",
+  user: users.sample,
+  category: Category.where(name: "Privé").first,
+  finish_at: DateTime.new(2016,12,2,19,0,0),
+  #proof: "",
+  #challenge_title: "",
+  #challenge: ""
+)
+ongoing_bet5.choices.create(
+  title: "Oui",
+  winning: false
+)
+ongoing_bet5.choices.create(
+  title: "Non",
+  winning: false
+)
+
+
+ongoing_bet6 = Bet.new(
+  title: "Vais-je tenir 100 jours sans manger ?",
+  user: users.sample,
+  category: Category.where(name: "Privé").first,
+  finish_at: DateTime.new(2017,3,12,12,0,0),
+  #proof: "",
+  challenge_title: "Passage de tondeuse",
+  challenge: "S'il ne tient pas les 100 jours, il devra se faire la boule à zéro mais s'il tient, c'est toi qui deviendra chauve !!"
+)
+ongoing_bet6.choices.create(
+  title: "Oui",
+  winning: false
+)
+ongoing_bet6.choices.create(
+  title: "Non",
+  winning: false
+)
+
+ongoing_bet7 = Bet.new(
+  title: "Brad Pitt va-t-il se taper Marion Cotillard avant 2017 ?",
+  user: users.sample,
+  category: Category.where(name: "Stars").first,
+  finish_at: DateTime.new(2017,1,1,0,0,0)
+)
+ongoing_bet7.choices.create(
+  title: "Oui",
+  winning: false
+)
+ongoing_bet7.choices.create(
+  title: "Non",
+  winning: false
+)
+
+
 ongoing_bets = [
-  Bet.create!(
-    title: "A quelle heure arrivera Sylvain le matin du Demo Day ?",
-    description: "Comme notre maître Ruby aime bien arriver après ses éleves, sera-t-il à l'heure le dernier jour pour le Demo Day",
-    user: users.sample,
-    category: Category.where(name: "Privé").first,
-    finish_at: DateTime.new(2016,12,2,9,0,1),
-    #proof: "",
-    challenge_title: "Le roux blond !",
-    challenge: "Si Sylvain arrive en retard (après 9h pétantes), il devra se teindre en blond"
-  ),
-  Bet.create!(
-    title: "Qui gagnera les présidentielles 2017 ?",
-    user: users.sample,
-    category: Category.where(name: "Politique").first,
-    finish_at: DateTime.new(2017,5,7,22,30,0)
-    #proof: ""
-  ),
-  Bet.create!(
-    title: "Julien est-il un black gay ?",
-    user: users.sample,
-    category: Category.where(name: "Privé").first,
-    finish_at: DateTime.new(2016,12,2,19,0,0),
-    #proof: "",
-    challenge_title: "Kiss kiss !",
-    challenge: "Si tu te trompes, tu devras embrasser Julien sur la bouche (dans n'importe quel cas) !"
-  ),
-  Bet.create!(
-    title: "Ronaldo va-t-il gagner le Ballon d'or ?",
-    user: users.sample,
-    category: Category.where(name: "Sports").first,
-    finish_at: DateTime.new(2017,9,1,19,0,0)
-  ),
-  Bet.create!(
-    title: "Yvon va-t-il se battre avec un Bulgare avant la fin de la formation ?",
-    user: users.sample,
-    category: Category.where(name: "Privé").first,
-    finish_at: DateTime.new(2016,12,2,19,0,0),
-    #proof: "",
-    #challenge_title: "",
-    #challenge: ""
-  ),
-  Bet.create!(
-    title: "Vais-je tenir 100 jours sans manger ?",
-    user: users.sample,
-    category: Category.where(name: "Privé").first,
-    finish_at: DateTime.new(2017,3,12,12,0,0),
-    #proof: "",
-    challenge_title: "Passage de tondeuse",
-    challenge: "S'il ne tient pas les 100 jours, il devra se faire la boule à zéro mais s'il tient, c'est toi qui deviendra chauve !!"
-  ),
-  Bet.create!(
-    title: "Brad Pitt va-t-il se taper Marion Cotillard avant 2017 ?",
-    user: users.sample,
-    category: Category.where(name: "Stars").first,
-    finish_at: DateTime.new(2017,1,1,0,0,0)
-  )
+  ongoing_bet1,
+  ongoing_bet2,
+  ongoing_bet3,
+  ongoing_bet4,
+  ongoing_bet5,
+  ongoing_bet6,
+  ongoing_bet7
 ]
+
+
+played_bet1 = Bet.new(
+  title: "Qui gagnera les élections US 2016 ?",
+  user: users.sample,
+  category: Category.where(name: "Politique").first,
+  finish_at: DateTime.new(2016,11,9,1,54,3)
+  #proof: ""
+  )
+played_bet1.choices.create(
+  title: "Hillary",
+  bet: played_bets[0],
+  winning: false
+  )
+played_bet1.choices.create(
+  title: "Donald",
+  bet: played_bets[0],
+  winning: true
+  )
+played_bet1.save
+
+
+played_bet2 = Bet.new(
+  title: "Qui passera au 2nd tour des primaires de la droite ?",
+  user: users.sample,
+  category: Category.where(name: "Politique").first,
+  finish_at: DateTime.new(2016,11,20,20,55,18)
+  #proof: ""
+  )
+played_bet2.choices.create(
+  title: "Juppé",
+  winning: true
+  )
+played_bet2.choices.create(
+  title: "Sarko",
+  winning: true
+  )
+played_bet2.choices.create(
+  title: "Fillon",
+  winning: true
+  )
+played_bet2.choices.create(
+  title: "Le Maire",
+  winning: true
+  )
+played_bet2.choices.create(
+  title: "Coppé",
+  winning: true
+  )
+played_bet2.save
 
 
 played_bets = [
-  Bet.create!(
-    title: "Qui gagnera les élections US 2016 ?",
-    user: users.sample,
-    category: Category.where(name: "Politique").first,
-    finish_at: DateTime.new(2016,11,9,1,54,3)
-    #proof: ""
-  ),
-  Bet.create!(
-    title: "Qui passera au 2nd tour des primaires de la droite ?",
-    user: users.sample,
-    category: Category.where(name: "Politique").first,
-    finish_at: DateTime.new(2016,11,20,20,55,18)
-    #proof: ""
-  )
-  # Bet.create!(
-  #   title: ,
-  #   description: ,
-  #   user: users.sample,
-  #   category: Category.where(name: "category").first,
-  #   finish_at: Faker::Time.between(1.day.ago, 7.days.ago),
-  #   proof: "",
-  #   challenge_title: "",
-  #   challenge: ""
-  # )
+  played_bet1,
+  played_bet2
 ]
 
-
-choices_of_bet1 = [
-  Choice.create!(
-    title: "Sylvain sera en avance (avant 9h)",
-    bet: ongoing_bets[0],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Sylvain sera à l'heure pile poils",
-    bet: ongoing_bets[0],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Sylvain sera en retard (comme d'hab)",
-    bet: ongoing_bets[0],
-    winning: false
-    )
-]
-choices_of_bet2 = [
-  Choice.create!(
-    title: "Fillon",
-    bet: ongoing_bets[1],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Juppé",
-    bet: ongoing_bets[1],
-    winning: false
-    ),
-  Choice.create!(
-    title: "MLP",
-    bet: ongoing_bets[1],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Mélenchon",
-    bet: ongoing_bets[1],
-    winning: false
-    )
-]
-choices_of_bet3 = [
-  Choice.create!(
-    title: "Oui",
-    bet: ongoing_bets[2],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Non",
-    bet: ongoing_bets[2],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Il est bi !",
-    bet: ongoing_bets[2],
-    winning: false
-    )
-]
-choices_of_bet4 = [
-  Choice.create!(
-    title: "Oui",
-    bet: ongoing_bets[3],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Non",
-    bet: ongoing_bets[3],
-    winning: false
-    )
-]
-choices_of_bet5 = [
-  Choice.create!(
-    title: "Oui",
-    bet: ongoing_bets[4],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Non",
-    bet: ongoing_bets[4],
-    winning: false
-    )
-]
-choices_of_bet6 = [
-  Choice.create!(
-    title: "Oui",
-    bet: ongoing_bets[5],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Non",
-    bet: ongoing_bets[5],
-    winning: false
-    )
-]
-choices_of_bet7 = [
-  Choice.create!(
-    title: "Oui",
-    bet: ongoing_bets[6],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Non",
-    bet: ongoing_bets[6],
-    winning: false
-    )
-]
-choices_of_bet8 = [
-  Choice.create!(
-    title: "Hillary",
-    bet: played_bets[0],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Donald",
-    bet: played_bets[0],
-    winning: true
-    )
-]
-choices_of_bet9 = [
-  Choice.create!(
-    title: "Juppé",
-    bet: played_bets[1],
-    winning: true
-    ),
-  Choice.create!(
-    title: "Sarko",
-    bet: played_bets[1],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Fillon",
-    bet: played_bets[1],
-    winning: true
-    ),
-  Choice.create!(
-    title: "Le Maire",
-    bet: played_bets[1],
-    winning: false
-    ),
-  Choice.create!(
-    title: "Coppé",
-    bet: played_bets[1],
-    winning: false
-    )
-]
-# choices_of_bet10 = [
-#   Choice.create!(
-#     title: ,
-#     bet: played_bets[2],
-#     winning: winning_status.sample
-#     ),
-#   Choice.create!(
-#     title: ,
-#     bet: played_bets[2],
-#     winning: winning_status.sample
-#     ),
-#   Choice.create!(
-#     title: ,
-#     bet: played_bets[2],
-#     winning: winning_status.sample
-#     )
-# ]
 
 
 users.each do |user|
