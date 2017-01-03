9997ef24a637197675f78813533424152708836f
Alexandre Brocard <a.brocard@ntymail.com>
2017-01-03 18:57:40 +0100

    sign-up page: 1st new version ('Se connecter avec' + buttons fb/twitter)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   has_many :bets, dependent: :destroy
   has_many :user_choices, dependent: :destroy
